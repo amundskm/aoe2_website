5e9154b9e44bca2ed14642a3dd7801499045bd53
Keith <amundskm@gmail.com>
2024-04-08 20:16:37 -0500

    Add Pages

---
+++ components/Navigation.tsx
@@ -0,0 +1,13 @@
+
+export default function Navigation() {
+    return(
+        <nav>
+            <ul>
+                <li>Leaderboard</li>
+                <li>Personal Stats</li>
+                <li>Individual Loopup</li>
+            </ul>
+        </nav>
+    )
+}
+
